f9b0371e4ca9c803401c4f0994b1d6c039be1502
Ong Kai Jin <47915713+OngKaiJin@users.noreply.github.com>
2025-07-19 21:12:32 +0800

    Update iabulkuploadtable.js

--- iabulkuploadtable.js
+++ iabulkuploadtable.js
@@ -1,21 +1,34 @@
 count = 0;
 suffix = "_mmu1.pdf";
+checked = [];
 imperfect = [];
 output = 'identifier,file,REMOTE_NAME,title,date,mediatype,collection\n';
 isinvalid = (id) => {
-    for (k of invalid.nopdf) {
-        if (id == k) {
+    for (j of invalid.nopdf) {
+        if (id == j) {
             return true;
         }
     }
 }
+isduplicate = (pdf) => {
+    count = 0;
+    for (j of checked) {
+        if (pdf == j) {
+            count = count + 1;
+        }
+    }
+    checked.push(pdf);
+    if (count) {
+        return true;
+    }
+}
 for (i of nodes) {
     citation = i.citation[0];
-    for (j of [["CLJ Rep", "CLJRep", "Current Law Journal Reprint", "cljrep"], ["CLJ", "CLJ", "Current Law Journal", "clj"]]) {
-        if (citation.includes(j[0])) {
-            citation = i.citation[0].replace(" " + j[0], "").replace("[", "").replace("]", "").split(" ");
-            cite = i.citation[0].replace(" " + j[0], "").replace("[", "").replace("]", "").replace("  ", " ").replaceAll(" ", "_");
-            testing = "/tempreport/" + j[1] + "_" + cite + suffix;
+    for (k of [["CLJ Rep", "CLJRep", "Current Law Journal Reprint", "cljrep"], ["CLJ", "CLJ", "Current Law Journal", "clj"]]) {
+        if (citation.includes(k[0])) {
+            citation = i.citation[0].replace(" " + k[0], "").replace("[", "").replace("]", "").split(" ");
+            cite = i.citation[0].replace(" " + k[0], "").replace("[", "").replace("]", "").replace("  ", " ").replaceAll(" ", "_");
+            testing = "/tempreport/" + k[1] + "_" + cite + suffix;
             if (testing != i.pdf) {
                 imperfect.push([testing, i.pdf, ('' + isinvalid(i.id)).replace("true", "not in table").replace("undefined", "in table")]);
             }
@@ -29,21 +42,21 @@ for (i of nodes) {
                 volume = '';
                 volume2 = '';
             }
-            identifier = j[3] + "_" + citation[0] + volume;
-            if (!isinvalid(i.id)) {
+            identifier = k[3] + "_" + citation[0] + volume;
+            if (!isinvalid(i.id) && !isduplicate(i.pdf)) {
                 if (previous != identifier) {
                     output += identifier;
                 }
                 output += ',"' + i.pdf.replace("/tempreport/", "").replace(suffix, "") + '"';
-                output += ',"' + i.pdf.replace("/tempreport/", "").replace(suffix, "") + ".pdf" + '"';
+                output += ',"' + i.pdf.replace("/tempreport/", "").replace(suffix, "").replaceAll("_", " ") + ".pdf" + '"';
                 if (previous != identifier) {
-                    output += ',"' + j[2] + ', ' + citation[0] + volume2 + '"';
-                    output += ',' + citation[0];
+                    output += ',"' + k[2] + ', ' + citation[0] + volume2 + '"';
+                    output += ',[' + citation[0] + ']';
                     output += ',texts';
                     output += ',current-law-journal';
                 }
+                previous = identifier;
             }
-            previous = identifier;
         }
     }
     output += '\n';
